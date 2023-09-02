ed668bea8b78f5c16e13e77a8e1e48e97ac150a7
William Wong <william.wai.lim.wong@gmail.com>
2023-09-02 19:53:17 -0400

    passes all tests

--- server/app.py
+++ server/app.py
@@ -19,15 +19,80 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+
+    if not animal:
+        response_body = '<h1>404 animal not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+
+    response_body = f'''
+        <ul>ID: {animal.id}</ul>
+        <ul>Name: {animal.name}</ul>
+        <ul>Species {animal.species}</ul>
+        <ul>Zookeeper: {animal.zookeeper.name}</ul>
+        <ul>Enclosure: {animal.enclosure.environment}</ul>
+    '''
+
+    response = make_response(response_body, 200)
+
+    return response
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+
+    if not zookeeper:
+        response_body = '<ul>404 zookeeper not found</ul>'
+        response = make_response(response_body, 404)
+        return response
+
+    response_body = f'''
+        <ul>ID: {zookeeper.id}</ul>
+        <ul>Name: {zookeeper.name}</ul>
+        <ul>Birthday: {zookeeper.birthday}</ul>
+    '''
+
+    animals = [animal for animal in zookeeper.animals]
+
+    if not animals:
+        response_body += f'<ul>Has no animals at this time.</ul>'
+
+    else:
+        for animal in animals:
+            response_body += f'<ul>Animal: {animal.name}</ul>'
+
+    response = make_response(response_body, 200)
+
+    return response
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+
+    if not enclosure:
+        response_body = '<ul>404 enclosure not found</ul>'
+        response = make_response(response_body, 404)
+        return response
+
+    response_body = f'''
+        <ul>ID: {enclosure.id}</ul>
+        <ul>Environment: {enclosure.environment}</ul>
+        <ul>Open to Visitors: {enclosure.open_to_visitors}</ul>
+    '''
+
+    animals = [animal for animal in enclosure.animals]
+
+    if not animals:
+        response_body += f'<ul>Has no animals at this time.</ul>'
+
+    else:
+        for animal in animals:
+            response_body += f'<ul>Animal: {animal.name}</ul>'
+
+    response = make_response(response_body, 200)
+
+    return response
 
 
 if __name__ == '__main__':
